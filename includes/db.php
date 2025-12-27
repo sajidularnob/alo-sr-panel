9a17c1e808db71c6bc098f8530ef46c441ab1109
sajidularnob <sajidhd1234@gmail.com>
2025-12-27 11:24:35 +0600

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,15 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "alo_sr_panel");
+$host = "localhost";
+$db   = "u400667647_alo";
+$user = "u400667647_alo_panel";
+$pass = "Yaharnob1234@";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
 if ($conn->connect_error) {
-    die("DB Connection Failed: " . $conn->connect_error);
+    die("Database connection failed: " . $conn->connect_error);
 }
+
+// Optional: set charset (recommended)
+$conn->set_charset("utf8mb4");
