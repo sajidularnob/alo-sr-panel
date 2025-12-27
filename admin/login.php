a0e45511a45f3e630c9c46c0d07dd9d22fdd6bf0
sajidularnob <sajidhd1234@gmail.com>
2025-12-27 13:04:53 +0600

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -12,7 +12,7 @@ $error = $_GET['error'] ?? '';
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Admin Login - Alo SR Panel</title>
+<title>Login - Alo SR Panel</title>
 <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen font-sans">
